e9de21159f6a9805635f729747789d62d7131eba
Bindiya Vakharia <161663134+bindiya-vakharia@users.noreply.github.com>
2026-06-18 20:31:32 -0400

    Add files via upload

---
+++ sql/create_table_query.sql
@@ -0,0 +1,42 @@
+ /* ==========================================
+CREATE TABLE QUERY
+========================================== */
+
+CREATE TABLE hr.employee_attrition (
+    age INTEGER,
+    attrition VARCHAR(10),
+    businesstravel VARCHAR(50),
+    dailyrate INTEGER,
+    department VARCHAR(50),
+    distancefromhome INTEGER,
+    education INTEGER,
+    educationfield VARCHAR(50),
+    employeecount INTEGER,
+    employeenumber INTEGER,
+    environmentsatisfaction INTEGER,
+    gender VARCHAR(20),
+    hourlyrate INTEGER,
+    jobinvolvement INTEGER,
+    joblevel INTEGER,
+    jobrole VARCHAR (100),
+    jobsatisfaction INTEGER,
+    maritalstatus VARCHAR(20),
+    monthlyincome INTEGER,
+    monthlyrate INTEGER,
+    numcompaniesworked INTEGER,
+    over18 VARCHAR(5),
+    overtime VARCHAR(10),
+    percentsalaryhike INTEGER,
+    performancerating INTEGER,
+    relationshipsatisfaction INTEGER,
+    standardhours INTEGER,
+    stockoptionlevel INTEGER,
+    totalworkingyears INTEGER,
+    trainingtimeslastyear INTEGER,
+    worklifebalance INTEGER,
+    yearsatcompany INTEGER,
+    yearsincurrentrole INTEGER,
+    yearssincelastpromotion
+ INTEGER,
+    yearswithcurrmanager INTEGER
+);
